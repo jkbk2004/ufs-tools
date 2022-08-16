c115d4cfd02af7d226e0dc419c37f32aec7f6cfd
JONG KIM <jong.kim@noaa.gov>
2022-08-16 09:34:44 -0400

    Update jet.sh

--- scripts/hpc-usage/jet.sh
+++ scripts/hpc-usage/jet.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+# We are on NOAA Jet
+if ( ! eval module help > /dev/null 2>&1 ) ; then
+    source /apps/lmod/lmod/init/bash
+fi
+module purge
+
 module load contrib noaatools
 
 shpcrpt -c jet -p h-nems
