d836aa9f0a4eeb08a438fc3fd9803439961f0a78
JONG KIM <jong.kim@noaa.gov>
2022-08-16 09:35:49 -0400

    Update orion.sh

--- scripts/hpc-usage/orion.sh
+++ scripts/hpc-usage/orion.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+# We are on Orion
+if ( ! eval module help > /dev/null 2>&1 ) ; then
+    source /apps/lmod/init/bash
+fi
+module purge
+
 module load contrib noaatools
 
 shpcrpt -c orion -p nems
